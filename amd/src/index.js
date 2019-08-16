5bf1d640edd3f4633cd6d55792c6c098c82f25fd
Oliver Kent <o.jkent@gmail.com>
2019-08-16 11:08:33 +0100

    Fixed JS error on LearnDEV

--- amd/src/index.js
+++ amd/src/index.js
@@ -1,5 +1,5 @@
 // Set the location select element as "required" if the user sets the assignment as a physical assignment.
-define(['jquery'], function() {
+define(['jquery'], function($) {
     function load() {
         document.getElementById("id_assignsubmission_physical_enabled").addEventListener("click", function() {
             if (document.getElementById("id_assignsubmission_physical_enabled").checked === true) {
@@ -7,17 +7,19 @@ define(['jquery'], function() {
             } else {
                 document.getElementById("id_location").removeAttribute("required");
             }
+
+        // SU_AMEND START - Assignment: Move element in focus beneath header
+            document.addEventListener('invalid', function(e){
+               $(e.target).addClass("invalid");
+               $('html, body').animate({scrollTop: $($(".invalid")[0]).offset().top - 80 }, 0);
+            }, true);
+            document.addEventListener('change', function(e){
+               $(e.target).removeClass("invalid");
+            }, true);
+        //SU_AMEND END
+
         }, false);
     }
-// SU_AMEND START - Assignment: Move element in focus beneath header
-    document.addEventListener('invalid', function(e){
-       $(e.target).addClass("invalid");
-       $('html, body').animate({scrollTop: $($(".invalid")[0]).offset().top - 80 }, 0);
-    }, true);
-    document.addEventListener('change', function(e){
-       $(e.target).removeClass("invalid");
-    }, true);
-//SU_AMEND END
 
     return {
         enhanceSettings: function() {
