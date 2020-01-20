04eb494276a29665fcce1417eb305a7debfdddaf
Catherine Newman <catherine.newman@solent.ac.uk>
2020-01-20 15:31:40 +0000

    hiding information about editing drafts

---
+++ amd/src/hide_edit_info.js
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+define(['jquery'], function($) {
+    return {
+        init: function() {
+            if ($( ".assignsubmission-physical-buttons" )[0]) {
+                $(".alert alert-info").css("display", "none");
+                $(".alert alert-info + p").css("clear", "both");
+            }
+        }
+    };
+});
