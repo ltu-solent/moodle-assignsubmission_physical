6571946d28e0db3778dbed0a1092446c954414cc
Mark Sharp <mark.sharp@solent.ac.uk>
2022-01-10 11:57:19 +0000

    Missing language string - physical

--- lang/en/assignsubmission_physical.php
+++ lang/en/assignsubmission_physical.php
@@ -83,6 +83,7 @@ $string['nodraftsubmission']      = 'no draft submission';
 $string['onduedate']              = ' (on the due date)';
 $string['participantheading']     = 'Student ID';
 $string['pdfimage']               = 'image of a pdf document';
+$string['physical'] = 'Physical';
 $string['physicalsubmissionsettingsheader'] = 'Physical submission settings';
 $string['pluginname']             = 'Physical Submissions';
 $string['privacy:metadata']       = 'The physical submission plugin does not store any personal data.';
